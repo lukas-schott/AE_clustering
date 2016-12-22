51ed7e5fd35eb8d298e9624aea2d8d9e7d63acc4
Thomas Hehn <ry386@stud.uni-heidelberg.de>
2016-12-22 11:04:34 +0100

    Added plots with colored points before and after spikes. Also added gitignore.

--- train_script.py
+++ train_script.py
@@ -19,16 +19,18 @@ def train_net():
     batch, _ = bm.get_batch()
     middle = functions['middle_f'](batch)
     out = functions['out_f'](batch)
+    loss = 0.0
 
     for iteration in range(n_iter):
         batch, _ = bm.get_batch()
         # print 'middle', middle
-        loss = functions['loss_f'](batch)
+        loss += functions['loss_f'](batch)
 
         if iteration % 10:
             bar.update(iteration)
         if iteration % 1000 == 0:
-            print '\r loss %.3f' % loss
+            print 'loss %.3f' % loss
+            loss = 0.0
 
         if iteration % 5000 == 0:
             bm.bs = 100000 - 10-1
@@ -42,7 +44,7 @@ def train_net():
             fig, ax = plt.subplots(2)
             ax[0].clear()
             ax[1].clear()
-            ax[0].scatter(xs, ys, alpha=0.3, c=spikes, marker=spikes)
+            ax[0].scatter(xs, ys, alpha=0.3, c=spikes) #, marker=spikes)
             # ax[0].scatter(xs[spikes == 0], ys[spikes == 0],  marker='^', alpha=0.3, c=(0, 0, 0))
             # ax[0].scatter(xs[spikes == 1], ys[spikes == 1],  marker='8',alpha=0.3, c=(0, 0.5, 0))
             # ax[0].scatter(xs[spikes == 2], ys[spikes == 2], marker='p',  alpha=0.3, c=(1, 0, 0))
@@ -55,10 +57,30 @@ def train_net():
             # ax[1].scatter(xsm[spikes == 0], ysm[spikes == 0],  marker='^', alpha=0.3, c=(0, 0, 0))
             # ax[1].scatter(xsm[spikes == 1], ysm[spikes == 1],  marker='8', alpha=0.3, c=(0, 0.5, 0))
             # ax[1].scatter(xsm[spikes == 2], ysm[spikes == 2], marker='p', alpha=0.3, c=(1, 0, 0))
-            fig.savefig('./../data/plots2/tDAllNormalplot_%i.png' % iteration)
+            fig.savefig('./../data/plots/tDAllNormalPlot_%i.png' % iteration)
             plt.close(fig)
-            bm.bs = bs
 
+            # shift values for patterns before/after actual pattern
+            sym_shifts = 7
+            shifted_spikes = np.concatenate((np.zeros(sym_shifts),
+                                             spikes,np.zeros(sym_shifts)))
+            no_shifts = sym_shifts*2+1
+            fig, axes = plt.subplots(3,5,figsize=(15,10))
+            for shift in range(no_shifts):
+              ax = axes.flatten()[shift]
+              ax.clear()
+              ax.set_title('Shift {}'.format(shift-sym_shifts))
+              for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                            ax.get_xticklabels() + ax.get_yticklabels()):
+                item.set_fontsize(10)
+              active_spikes = shifted_spikes[shift:
+                                             shift+spikes.shape[0]]
+              ax.scatter(xs, ys, alpha=0.3, c=active_spikes)
+            fig.tight_layout()
+            fig.savefig('./../data/plots/2DAllShiftPlot_%i.png' % iteration)
+            plt.close(fig)
+ 
+            bm.bs = bs
 
     bm.bs = 10000
     batch = bm.get_batch()
